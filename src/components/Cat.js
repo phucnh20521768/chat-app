4566e5e24db9fe7b0dfcf7f84442d20116da4bca
phucnh20521768 <nhphuc0703@gmail.com>
2024-06-07 10:34:14 +0700

    Responsive UI

---
+++ src/components/Cat.js
@@ -0,0 +1,13 @@
+import { Avatar } from "@mui/material";
+import React from "react";
+
+const Cat = () => {
+  return (
+    <Avatar
+      src="https://encrypted-tbn3.gstatic.com/images?q=tbn:ANd9GcSGfpQ3m-QWiXgCBJJbrcUFdNdWAhj7rcUqjeNUC6eKcXZDAtWm"
+      alt="cat"
+    />
+  );
+};
+
+export default Cat;
